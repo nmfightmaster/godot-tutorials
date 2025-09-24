523dacb1c7c5ccc77926f4c204f43c54745c20e1
Nathan <nmfightmaster@gmail.com>
2025-09-24 15:29:25 -0700

    collision detection

--- project-boost/scripts/player.gd
+++ project-boost/scripts/player.gd
@@ -2,9 +2,17 @@ extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * 1000)
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0, 0, 100 * delta))
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0, 0, -100 * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print("You win!")
+	if "Hazard" in body.get_groups():
+		print("You lose!")
+
