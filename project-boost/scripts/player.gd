9ef83dca8d4155c9b17822f463f406a77be497ac
Nathan <nmfightmaster@gmail.com>
2025-09-24 17:08:32 -0700

    export variables, SceneTree

--- project-boost/scripts/player.gd
+++ project-boost/scripts/player.gd
@@ -1,18 +1,31 @@
 extends RigidBody3D
 
+## Amount of vertical thrust applied when moving.
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+
+## Amount of torque applied when rotating.
+@export var torque_thrust: float = 100.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000)
+		apply_central_force(basis.y * delta * thrust)
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0, 0, 100 * delta))
+		apply_torque(Vector3(0, 0, torque_thrust * delta))
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0, 0, -100 * delta))
+		apply_torque(Vector3(0, 0, -torque_thrust * delta))
 
 
 func _on_body_entered(body: Node) -> void:
 	if "Goal" in body.get_groups():
-		print("You win!")
+		complete_level()
 	if "Hazard" in body.get_groups():
-		print("You lose!")
+		crash_sequence()
 
+func crash_sequence() -> void:
+	print("KABOOM!")
+	get_tree().call_deferred("reload_current_scene")
+
+func complete_level() -> void:
+	print("Level Complete!")
+	get_tree().quit()
